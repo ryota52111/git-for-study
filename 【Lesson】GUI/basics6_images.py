6c619080087bade91958e3dedcf40cd40b23246b
ryota52111 <ryota37111@icloud.com>
2020-10-14 17:05:35 +0900

    GUI_stady_20201014

---
+++ 【Lesson】GUI/basics6_images.py
@@ -0,0 +1,37 @@
+#Images
+import tkinter
+from PIL import ImageTk, Image
+
+#Define window
+root = tkinter.Tk()
+root.title("Image Basics!")
+root.iconbitmap("thinking.ico")
+root.geometry("700x700")
+
+#Define functions
+def make_image():
+    '''print of Image'''
+    global cat_image
+    #Using PIL for jpg
+    cat_image = ImageTk.PhotoImage(Image.open("image_bicycle.jpg"))
+    cat_label = tkinter.Label(root, image= cat_image)
+    cat_label.pack()
+
+#Basics...works for png
+my_image = tkinter.PhotoImage(file="shield.png")
+my_label = tkinter.Label(root, image=my_image)
+my_label.pack()
+
+my_button = tkinter.Button(root, image=my_image)
+my_button.pack()
+
+#Not for jpeg
+# cat_image = tkinter.PhotoImage(file="image_bicycle.jpg")
+# cat_label = tkinter.Label(root, image=cat_image)
+# cat_label.pack()
+
+make_image()
+
+
+#Run root windwos's main loop
+root.mainloop()
